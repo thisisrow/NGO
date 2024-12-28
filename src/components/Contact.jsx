2fa958b1071482ca865249c0a9742155edb63995
thisisrow <prathameshmishra2020@gmail.com>
2024-12-28 14:51:26 +0530

    Refactor emailjs environment variable references in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,15 +23,15 @@ const Contact = () => {
     setLoading(true);
     emailjs
       .send(
-        import.meta.env.EMAILJS_SERVICE_ID,
-        import.meta.env.EMAILJS_TEMPLATE_ID,
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           from_email: form.email,
           phone: form.phone,
           message: form.message,
         },
-        import.meta.env.EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
